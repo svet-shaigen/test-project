e6c7faf24573125e5c1f793af68e4cce6830b005
svet-shaigen <110323981+svet-shaigen@users.noreply.github.com>
2023-04-03 09:59:16 +0300

    Updata

---
+++ src/fetchImages.js
@@ -0,0 +1,16 @@
+// import axios from "axios";
+
+// const BASE_URL = 'https://pixabay.com/api/';
+// const KEY = '34927401-121899480aec54049d4a80921';
+
+// async function fetchImages(query) {
+//     try {
+//     const respons = await axios.get(`${BASE_URL}?key=${KEY}&q=${query}&
+//     image_type=photo&orientation=horizontal&safesearch=true`);
+//     return respons.data;
+// } catch (error) {
+//     Notiflix.Notify.failure('Something went wrong. Please try again later.');
+//     return Promise.reject(error);
+//   }
+// }
+
