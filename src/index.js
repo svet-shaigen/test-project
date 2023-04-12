980fafc26fd730bb14c023b68d8c24d856dfdb17
svet-shaigen <110323981+svet-shaigen@users.noreply.github.com>
2023-04-12 14:45:22 +0300

    AddrenderHtml.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
-import getPokeApi from "./js/getPokeApi.js"
+import {getPokeApi,getPokemonName} from "./js/getPokeApi.js"
+import renderHtml from "./js/renderHtml.js";
 
-getPokeApi()
+let name = ''
 
+const showPokemons = document.querySelector('.show-pokemons');
+showPokemons.addEventListener('click', onShowPokemons);
+
+function onShowPokemons() {
+    getPokeApi()
+    .then(dataPokemons => {
+        let pokemon = dataPokemons.data.results
+    })
+    .catch(error => {
+        console.log(error)
+    }
+    )
+}
+
+
+
+getPokemonName(name)
+.then(dataPokemon => {
+    //  console.log(dataPokemon)
+})
+.catch(error => {
+    // console.log(error)
+}
+)
--- src/js/getPokeApi.js
+++ src/js/getPokeApi.js
@@ -2,13 +2,26 @@ import axios from "axios";
 
 const BASE_URL = 'https://pokeapi.co/api/v2/';
 
-export default function getPokeApi() {
+ function getPokeApi() {
     const URL = `${BASE_URL}pokemon?limit=10&offset=0`
     return axios.get(URL)
     .then(response => {
-         console.log(response)
+        if(!response.status === 200)
+            throw new Error(response.status)
+         return response;
      })
  }
 
- 
+ function getPokemonName(name) {
+    const URL_NAME = `${BASE_URL}pokemon/${name}`
+    return axios.get(URL_NAME)
+    .then(response => {
+        if(!response.status === 200)
+            throw new Error(response.status)
+         return response;
+     })
+
+ }
+
+ export {getPokeApi,getPokemonName}
  
